2c147e4ebfa82b98aec8f30cada509aeb1a375ff
emptyhead <korea6370@yu.ac.kr>
2023-08-04 14:49:30 +0900

    23/8/4 update

---
+++ ClassesDemo/ClassesDemo/main.swift
@@ -0,0 +1,13 @@
+
+let skeleton1 = Enemy(health: 100, attackStrength: 10)
+let skeleton2 = skeleton1
+
+skeleton1.takeDamage(amount: 10)
+
+print(skeleton2.health)
+
+skeleton1.takeDamage(amount: 10)
+skeleton2.takeDamage(amount: 10)
+
+print(skeleton1.health)
+print(skeleton2.health)
