83ea746415cc87e09ddc2fe1160e1fde84763aab
emptyhead <korea6370@yu.ac.kr>
2023-08-07 10:06:28 +0900

    23/8/7 update

---
+++ Closures/Closures/main.swift
@@ -0,0 +1,7 @@
+
+import Foundation
+
+let array = [6, 2, 3, 9, 4, 1]
+
+let newArray = array.map{"\($0)"}
+print(newArray)
---
+++ WeatherManager.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherManager.swift
+//  Clima
+//
+//  Created by KIM Hyung Jun on 2023/08/06.
+//  Copyright © 2023 App Brewery. All rights reserved.
+//
+
+import Foundation
