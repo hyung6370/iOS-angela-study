5ac0709456fa644adc470c4f50a2d277c2b25c07
emptyhead <korea6370@yu.ac.kr>
2023-08-24 17:28:20 +0900

    23/8/24 update

---
+++ CoreDataTest/CoreDataTest/Data Model/Item.swift
@@ -0,0 +1,13 @@
+//
+//  Item.swift
+//  CoreDataTest
+//
+//  Created by KIM Hyung Jun on 2023/08/22.
+//
+
+import Foundation
+
+class Item: Codable {
+    var title: String = ""
+    var done: Bool = false
+}
---
+++ CoreDataTest/CoreDataTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataTest
+//
+//  Created by KIM Hyung Jun on 2023/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
