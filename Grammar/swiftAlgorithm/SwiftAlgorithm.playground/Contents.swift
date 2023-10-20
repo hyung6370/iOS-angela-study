719d5e08cbe19a0923e4719287867a513eafab32
emptyhead <korea6370@yu.ac.kr>
2023-10-20 22:11:16 +0900

    23/10/20 update

--- Grammar/swiftAlgorithm/SwiftAlgorithm.playground/Contents.swift
+++ Grammar/swiftAlgorithm/SwiftAlgorithm.playground/Contents.swift
@@ -1,40 +1,37 @@
-import UIKit
+import Foundation
 
-let s = "abcdef"
-let x = s[s.index(s.startIndex, offsetBy: 3)]
-// x의 타입은 String이 아닌 Character
+// 시간 측정 메서드
+public func progressTime(_ closure: () -> ()) -> TimeInterval {
+    let start = CFAbsoluteTimeGetCurrent()
+    closure()
+    let diff = CFAbsoluteTimeGetCurrent() - start
+    return (diff)
+}
 
-// 배열과 문자열의 인덱스 접근법 비교
-let arr = [0, 1, 2, 3, 4]
-let str = "abcde"
+// 긴 배열
+var arr = [Int](repeating: 0, count: Int(pow(10.0, 6)))
 
-// 배열은 편하게 인덱스 접근 가능
-let arr2 = arr[0..<3]
-print(arr2)
+// arr의 마지막 4개의 원소를 pop한 배열의 결과를 얻고 싶다
 
-// 문자열은 편하게 인덱스 접근이 불가능. String.Index 형으로 접근해야 한다
-let i = str.startIndex
-let j = str.index(i, offsetBy: 3)
-let str2 = str[i..<j]
-print(i)
-print(j)
-print(arr2)
-print(str2)
+// 1. dropLast 한 것을 대입 -> 0.35초
+progressTime {
+    let arr2 = arr.dropLast(4)
+}
 
-let s5 = s[s.startIndex ..< s.index(s.startIndex, offsetBy: 3)]
-// "abc"
-// Swift의 Index로 자른 결과는 String이 아닌 SubString 타입
-var stringList = [String]()
-stringList.append(String(s5)) // 타입 변환 필요
+// 2. index 슬라이싱 대입 -> 0.32초
+progressTime {
+    let arr2 = arr[arr.startIndex..<arr.index(arr.startIndex, offsetBy: arr.endIndex - 4)]
+}
 
+// 3. popLast() 4번 -> 0.0002초
+progressTime {
+    arr.popLast()
+    arr.popLast()
+    arr.popLast()
+    arr.popLast()
+}
 
-
-//let s = "abcdef"
-//let s1 = s.prefix(3)
-//let s2 = s.suffix(3)
-//let s3 = s.dropFirst(3)
-//let s4 = s.dropLast(3)
-//
-//let s5 = s.prefix(100)
-//
-//print(s1, s2, s3, s4, s5)
+// 4. removeLast(4) -> 0.0007초
+progressTime {
+    arr.removeLast(4)
+}
